a8725315926cfec3ee5dfb8a5f72809e7dffbf91
Игорь Дружинин <igordruzinin@air-igor.local>
2023-03-29 12:52:25 +0300

    first commit

---
+++ app/src/main/java/com/example/reminders20/ListFragment.java
@@ -0,0 +1,19 @@
+package com.example.reminders20;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/reminders20/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.reminders20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getFragmentManager().beginTransaction().replace(R.id.container, new ListFragment(), "fragment1").commit();
+    }
+}
